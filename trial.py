3cafa166a4b167e0293ee7f8fdd8bc5ad528a845
nejcik <nataliaxskowronek@gmail.com>
2019-01-30 09:49:03 +0100

    Trial - outliers

---
+++ trial.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+outliers=[]
+
+def detect_outlier(data_1):
+    
+    threshold=3
+    mean_1 = np.mean(data_1)
+    std_1 =np.std(data_1)
+    
+    
+    for y in data_1:
+        z_score= (y - mean_1)/std_1 
+        if np.abs(z_score) > threshold:
+            outliers.append(y)
+    return outliers
+
+happy = pd.read_csv("_data/2017.csv")
+print happy 
+
+outlier_datapoints = detect_outlier(happy)
+print(outlier_datapoints)
